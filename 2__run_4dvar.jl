4fbfdda33f747b4908a7c177f47e37da2fc34694
John Waczak <john.louis.waczak@gmail.com>
2023-06-10 20:45:57 -0500

    fixed missing measurement issue

--- 2__run_4dvar.jl
+++ 2__run_4dvar.jl
@@ -205,7 +205,12 @@ is_meas_in_mechanism = [spec ∈ df_species[!, "MCM Name"] for spec ∈ names(df
 
 idx_ts_to_use = df_params.t .≤ 0.0
 df_nd_to_use = df_number_densities[idx_ts_to_use, Not([measurements_to_ignore..., :t, :w_ap])]
+df_nd_to_use = df_nd_to_use[:, is_meas_in_mechanism]
+
 df_nd_to_use_ϵ = df_number_densities_ϵ[idx_ts_to_use, Not([measurements_to_ignore..., :t, :w_ap])]
+df_nd_to_use_ϵ = df_nd_to_use_ϵ[:, is_meas_in_mechanism]
+
+
 
 for spec_name ∈ names(df_nd_to_use)
     println(spec_name)
